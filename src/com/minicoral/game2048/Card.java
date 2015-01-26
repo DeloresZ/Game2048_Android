172df17b632e2a9fb6a27a709ed99e5551b61e2d
ZhangXin <sanjin6035@hotmail.com>
2015-01-26 12:22:43 +0800

    First commit

---
+++ src/com/minicoral/game2048/Card.java
@@ -0,0 +1,52 @@
+package com.minicoral.game2048;
+
+import android.R.integer;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class Card extends FrameLayout
+{
+
+	private int num = 0;
+	private TextView label;
+
+	public Card(Context context)
+	{
+		super(context);
+		label = new TextView(getContext());
+		label.setBackgroundColor(0x33ffffff);
+		label.setTextSize(35);
+		label.setGravity(Gravity.CENTER);
+		LayoutParams lp = new LayoutParams(-1, -1);
+		lp.setMargins(20, 20, 0, 0);
+		addView(label, lp);
+		setNum(0);
+	}
+
+	public int getNum()
+	{
+		return num;
+	}
+
+	public void setNum(int num)
+	{
+		this.num = num;
+		if (num<=0)
+		{
+			label.setText("");
+		}
+		else {
+			label.setText(num + "");
+		}
+	}
+
+	public boolean equals(Card o)
+	{
+		// TODO Auto-generated method stub
+		return getNum() == o.getNum();
+	}
+	
+}
---
+++ src/com/minicoral/game2048/MainActivity.java
@@ -0,0 +1,155 @@
+package com.minicoral.game2048;
+
+import net.youmi.android.AdManager;
+import net.youmi.android.banner.AdSize;
+import net.youmi.android.banner.AdView;
+import net.youmi.android.banner.AdViewListener;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity
+{
+	public MainActivity()
+	{
+		mainActivity = this;
+	}
+
+	private TextView tvScore;
+	private int score = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// SpotManager.getInstance(this).showSplashSpotAds(this,
+		// MainActivity.class);
+		tvScore = (TextView) findViewById(R.id.tvSocre);
+		AdManager.getInstance(this).init("2aa219599aa11756",
+				"3eac5cf83f121b28", false);
+
+		LinearLayout adLayout = (LinearLayout) findViewById(R.id.adLayout);
+		AdView adView = new AdView(this, AdSize.FIT_SCREEN);
+		adLayout.addView(adView);
+		// 实例化广告条
+		// AdView adView = new AdView(this, AdSize.FIT_SCREEN);
+		//
+		// // 获取要嵌入广告条的布局
+		// LinearLayout adLayout = (LinearLayout) findViewById(R.id.adLayout);
+		//
+		// // 将广告条加入到布局中
+		// adLayout.addView(adView);
+		// 监听广告条接口
+		adView.setAdListener(new AdViewListener()
+		{
+
+			@Override
+			public void onSwitchedAd(AdView arg0)
+			{
+				Log.i("YoumiAdDemo", "广告条切换");
+			}
+
+			@Override
+			public void onReceivedAd(AdView arg0)
+			{
+				Log.i("YoumiAdDemo", "请求广告成功");
+
+			}
+
+			@Override
+			public void onFailedToReceivedAd(AdView arg0)
+			{
+				Log.i("YoumiAdDemo", "请求广告失败");
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void clearScore()
+	{
+		score = 0;
+		showScore();
+	}
+
+	public void addScore(int s)
+	{
+		score += s;
+		showScore();
+	}
+
+	public void showScore()
+	{
+		tvScore.setText(score + "");
+	}
+
+	public static MainActivity mainActivity = null;
+
+	public static MainActivity getMainActivity()
+	{
+		return mainActivity;
+	}
+
+	private void showBanner()
+	{
+
+		// 广告条接口调用（适用于应用）
+		// 将广告条adView添加到需要展示的layout控件中
+		// LinearLayout adLayout = (LinearLayout) findViewById(R.id.adLayout);
+		// AdView adView = new AdView(this, AdSize.FIT_SCREEN);
+		// adLayout.addView(adView);
+
+		// 广告条接口调用（适用于游戏）
+
+		// 实例化LayoutParams(重要)
+		FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
+				FrameLayout.LayoutParams.WRAP_CONTENT,
+				FrameLayout.LayoutParams.WRAP_CONTENT);
+		// 设置广告条的悬浮位置
+		layoutParams.gravity = Gravity.BOTTOM | Gravity.RIGHT; // 这里示例为右下角
+		// 实例化广告条
+		AdView adView = new AdView(this, AdSize.FIT_SCREEN);
+		// 调用Activity的addContentView函数
+
+		// 监听广告条接口
+		adView.setAdListener(new AdViewListener()
+		{
+
+			@Override
+			public void onSwitchedAd(AdView arg0)
+			{
+				Log.i("YoumiAdDemo", "广告条切换");
+			}
+
+			@Override
+			public void onReceivedAd(AdView arg0)
+			{
+				Log.i("YoumiAdDemo", "请求广告成功");
+
+			}
+
+			@Override
+			public void onFailedToReceivedAd(AdView arg0)
+			{
+				Log.i("YoumiAdDemo", "请求广告失败");
+			}
+		});
+		this.addContentView(adView, layoutParams);
+	}
+
+}
